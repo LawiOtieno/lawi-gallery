635ca4a925c5de06fd52f431e82cbc853c22ff97
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-08 19:15:22 +0300

    categories interpolated

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import Category,Photos
 
 import photos
 
 # Create your views here.
 
 def gallery(request):
-    return render(request, 'all-photos/gallery.html')
+    categories = Category.objects.all()
+
+    
+    context = {'categories':categories}
+    return render(request, 'all-photos/gallery.html',context)
 
 def viewPhotos(request, pk): # pk
     return render(request, 'all-photos/photos.html')
