09a46a535159d2e2f2c432febd8404719b4ac11f
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-08 18:41:19 +0300

    add photos - models

--- personalGallery/settings.py
+++ personalGallery/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'personalGallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'personalgalleries',
+        'USER': 'access',
+        'PASSWORD':'lawioti',
+        # 'HOST':'localhost' Lawi1243
     }
 }
 
@@ -108,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -121,6 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS =[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.deletion import SET_NULL
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
 
@@ -8,3 +10,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category_name
+
+class Photos(models.Model):
+    category = models.ForeignKey(Category,on_delete=SET_NULL,null=True)
+    image = ImageField(null=False,blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
