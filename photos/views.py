1e8ebb9268432ed7fe218368241efc38ce3c5e76
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-08 20:03:00 +0300

    connect photos & details

--- photos/views.py
+++ photos/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
 from .models import Category,Photos
 
-import photos
+# import photos
 
 # Create your views here.
 
 def gallery(request):
     categories = Category.objects.all()
+    photos = Photos.objects.all()
 
-    
-    context = {'categories':categories}
+    context = {'categories':categories, 'photos':photos}
     return render(request, 'all-photos/gallery.html',context)
 
 def viewPhotos(request, pk): # pk
-    return render(request, 'all-photos/photos.html')
+    photo = Photos.objects.get(id=pk)
+
+    return render(request, 'all-photos/photos.html',{'photo':photo})
 
 def addPhotos(request):
     return render(request, 'all-photos/add-photos.html')
