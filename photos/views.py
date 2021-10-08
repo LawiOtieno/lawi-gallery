dcfa582802426c1b38d21abcb27dcfc9c3ecbec5
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-10-09 00:37:34 +0300

    create add photo

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+# import photos
 from .models import Category,Photos
 
 # import photos
@@ -6,8 +8,14 @@ from .models import Category,Photos
 # Create your views here.
 
 def gallery(request):
+    category=request.GET.get('category')
+    if category==None:
+        photos=Photos.objects.all()
+    else:
+        photos=Photos.objects.filter(category__category_name=category)
+
+
     categories = Category.objects.all()
-    photos = Photos.objects.all()
 
     context = {'categories':categories, 'photos':photos}
     return render(request, 'all-photos/gallery.html',context)
@@ -18,5 +26,28 @@ def viewPhotos(request, pk): # pk
     return render(request, 'all-photos/photos.html',{'photo':photo})
 
 def addPhotos(request):
-    return render(request, 'all-photos/add-photos.html')
+    categories = Category.objects.all()
+
+    if request.method=='POST':
+        data=request.POST
+        image=request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['new_category'] != '':
+            category,created=Category.objects.get_or_create(category_name=data['new_category'])
+        else:
+            category=None
+
+
+        photo = Photos.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+        return redirect
+
+
+    context = {'categories':categories}
+    return render(request, 'all-photos/add-photos.html',context)
 
